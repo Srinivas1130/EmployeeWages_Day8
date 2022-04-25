f2eaf66712c9c5358b00ba24f1f44cbbaab97242
Srinivas1130 <srinivasachar1998@gmail.com>
2022-04-25 08:12:31 +0530

    Welcome Employee Wages code

---
+++ EmployeeWageUC1.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.day8Assignment;
+
+public class EmployeeWageUC1 {
+	public void Welcome(){
+		System.out.println("Welcome to Employee Wage Computation");
+		
+	}
+
+      public static void main(String[] args) {
+	     EmployeeWageUC1 employeeWageUC1=new EmployeeWageUC1();
+	     employeeWageUC1.Welcome();
+	}
+}
